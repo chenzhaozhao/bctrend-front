20950803b63247259f96a385bcae7f0742548fc8
zane.chen <chenchaochao@bbinsure.cn>
2022-07-18 17:00:36 +0800

    update deribit

--- pages/deribit/index.tsx
+++ pages/deribit/index.tsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import { Line, DualAxes } from "@ant-design/plots";
 import request from "../../utils/request";
 import dayjs from "dayjs";
-
-const Home = () => {
+const Home = ({dataSource}:{dataSource:[[],[]]}) => {
   const [data, setData] = useState<any[]>([[], []]);
   const [range, setRange] = useState([
     { min: 0, max: 100 },
@@ -11,30 +10,34 @@ const Home = () => {
   ]);
   useEffect(() => {
     (async () => {
-      const data: any = await request({ url: "/deribit?func=24hrvolume" });
-      const data1: any = await request({ url: "/deribit?func=openinterest" });
-      const ratio = data1.result?.map((item: any) => ({
+      const [data,data1]=dataSource;
+      const ratio = data1?.map((item: any) => ({
         count: parseFloat(item.ratio),
         type: "Ratio",
         time: dayjs(item.time).format("YYYY-MM-DD HH:mm"),
       }));
-      const ratio1 = data.result?.map((item: any) => ({
+      const ratio1 = data?.map((item: any) => ({
         value: parseFloat(item.ratio1),
         type: "Ratio1",
         time: dayjs(item.time).format("YYYY-MM-DD HH:mm"),
       }));
-      const ratio2 = data.result?.map((item: any) => ({
+      const ratio2 = data?.map((item: any) => ({
         value: parseFloat(item.ratio2),
         type: "Ratio2",
         time: dayjs(item.time).format("YYYY-MM-DD HH:mm"),
       }));
 
-      const values = ratio.map(({count}:{count:number}) => count);
-      const values1 = [...ratio1,...ratio2].map(({value}:{value:number}) => value);
-      setRange([{ min:Math.min(...values), max:Math.max(...values) },{min:Math.min(...values1),max:Math.max(...values1)}]);
+      const values = ratio.map(({ count }: { count: number }) => count);
+      const values1 = [...ratio1, ...ratio2].map(
+        ({ value }: { value: number }) => value
+      );
+      setRange([
+        { min: Math.min(...values), max: Math.max(...values) },
+        { min: Math.min(...values1), max: Math.max(...values1) },
+      ]);
       setData([ratio, [...ratio1, ...ratio2]]);
     })();
-  }, []);  //   //   console.log(data, "9999");
+  }, []); //   //   console.log(data, "9999");
   return (
     <div
       className=" h-full py-8 px-8 mx-6 my-10 rounded-sm"
@@ -70,12 +73,12 @@ const Home = () => {
           {
             geometry: "line",
             seriesField: "type",
-            ...range[0]
+            ...range[0],
           },
           {
             geometry: "line",
             seriesField: "type",
-            ...range[1]
+            ...range[1],
           },
         ]}
         height={700}
@@ -88,5 +91,21 @@ const Home = () => {
     </div>
   );
 };
-
+export async function getServerSideProps() {
+  try {
+    const data: any = await Promise.all([
+      request({ url: "/deribit?func=24hrvolume" }),
+      request({ url: "/deribit?func=openinterest" }),
+    ]);
+    return {
+      props: {
+        dataSource: data.map(({ result }: { result: any[] }) => result),
+      },
+    };
+  } catch (error) {
+    return {
+      props: { data: [] },
+    };
+  }
+}
 export default Home;
